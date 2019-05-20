cb8b47fb5c5ba8f073df8638f46ca55d0781ee36
cug_xy <cugxy@qq.com>
2019-05-20 08:56:18 +0800

    proj 文件

---
+++ scripts/leetcode/97_inter_leave.py
@@ -0,0 +1,26 @@
+#!usr/bin/env python  
+# -*- coding:utf-8 _*-
+"""
+给定三个字符串 s1, s2, s3, 验证 s3 是否是由 s1 和 s2 交错组成的。
+
+示例 1:
+    输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+    输出: true
+示例 2:
+    输入: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+    输出: false
+"""
+from IPython import embed
+
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        
+
+if __name__ == '__main__':
+    pass

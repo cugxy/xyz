cb3b693e7e90298a08e6ac6aaab8201600eb6f92
cug_xy <cugxy@qq.com>
2019-05-24 18:27:49 +0800

    97 题 动态规划 ????

--- scripts/leetcode/97_inter_leave.py
+++ scripts/leetcode/97_inter_leave.py
@@ -11,6 +11,8 @@
     输出: false
 """
 from IPython import embed
+import numpy as np
+
 
 class Solution(object):
     def isInterleave(self, s1, s2, s3):
@@ -20,7 +22,30 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
-        
+        l1, l2, l3 = len(s1), len(s2), len(s3)
+        if not l3 == l1 + l2:
+            return False
+        dp = np.zeros((l1+1, l2+1))
+        for i in range(l1+1):
+            for j in range(l2+1):
+                if i == 0 and j == 0:
+                    dp[i, j] = 1
+                elif i > 0 and dp[i - 1, j] and s3[i+j-1] == s1[i - 1]:
+                    dp[i, j] = 1
+                elif j > 0 and dp[i, j - 1] and s3[i+j-1] == s2[j - 1]:
+                    dp[i, j] = 1
+                else:
+                    dp[i, j] = 0
+        return True if dp[l1, l2] else False
+
 
 if __name__ == '__main__':
+    s = Solution()
+    s1 = 'aabcc'
+    s2 = 'dbbca'
+    s3 = 'aadbbcbcac'
+    s4 = 'aadbbbaccc'
+
+    r = s.isInterleave(s1, s2, s4)
+    print(r)
     pass

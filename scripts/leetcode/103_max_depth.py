16342fbeaecfc024a96888ff91ed6c3388f8d4eb
cugxy <cugxy@qq.com>
2019-06-01 19:05:05 +0800

    leetcode 103

---
+++ scripts/leetcode/103_max_depth.py
@@ -0,0 +1,56 @@
+#!usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@author:$USER
+@file: $NAME 
+@time: $YEAR/$MONTH/$DAY
+"""  
+"""
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+    给定二叉树 [3,9,20,null,null,15,7]，
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    返回它的最大深度 3 。
+"""
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+if __name__ == '__main__':
+    s = Solution()
+    n3 = TreeNode(3)
+    n9 = TreeNode(9)   
+    n20 = TreeNode(20)
+    n15 = TreeNode(15)
+    n7 = TreeNode(7)
+    n3.left = n9
+    n3.right = n20
+    n20.left = n15
+    n20.right = n7
+    r = s.maxDepth(n3)
+    print(r)
+
+

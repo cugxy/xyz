b8ff152c9cf398cd287468044435583953c919d0
cugxy <cugxy@qq.com>
2019-07-23 21:54:28 +0800

    leetcode 145 new LRU

--- scripts/leetcode/145LRU.py
+++ scripts/leetcode/145LRU.py
@@ -40,7 +40,7 @@ cache.get(4);       // 返回  4
 """
 
 
-class LRUCache(object):
+class LRUCache1(object):
 
     def __init__(self, capacity):
         """
@@ -92,6 +92,84 @@ class LRUCache(object):
 # obj.put(key,value)
 # ["LRUCache","put","put","put","put","get","get"]
 # [[2],[2,1],[1,1],[2,3],[4,1],[1],[2]]
+
+
+class ListNode(object):
+    def __init__(self, key=None, val=None):
+        self.key = key
+        self.val = val
+        self.pre = None
+        self.next = None
+
+    def __repr__(self):
+        return 'ListNode key: %s | val: %s' % (self.key, self.val, )
+
+
+class LRUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.count = 0
+        self.cap = capacity
+        self.data = {}              # key <-> ListNode
+        self.head = ListNode('head', 'head')
+        self.tail = ListNode('tail', 'tail')
+        self.head.next = self.tail
+        self.tail.pre = self.head
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        node = self.data.get(key, None)
+        if not node:
+            return -1
+        self._move_to_head(node)
+        return node.val
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        node = self.data.get(key, None)
+        if node:
+            node.val = value
+            self._move_to_head(node)
+            return
+        node = ListNode(key, value)
+        self.data[key] = node
+        self._add(node)
+        self.count += 1
+        if self.count > self.cap:
+            tail = self._pop_node()
+            self.data.pop(tail.key)
+            self.count -= 1
+
+    def _add(self, node):
+        node.pre = self.head
+        node.next = self.head.next
+        self.head.next.pre = node
+        self.head.next = node
+
+    def _remove(self, node):
+        pre = node.pre
+        next = node.next
+        pre.next = next
+        next.pre = pre
+
+    def _move_to_head(self, node):
+        self._remove(node)
+        self._add(node)
+
+    def _pop_node(self):
+        res = self.tail.pre
+        self._remove(res)
+        return res
+
 
 if __name__ == '__main__':
     cache = LRUCache(2)

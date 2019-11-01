2d6836b435632aec5dff72dc5a7027465c798543
cug_xy <cugxy@qq.com>
2019-11-01 15:12:25 +0800

    2019 11 问题

---
+++ learn/python/coroutines_tasks.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：       coroutines_tasks.py
+   Description :
+   Author :          cugxy
+   date：            2019/11/1
+-------------------------------------------------
+   Change Activity:
+                     2019/11/1
+-------------------------------------------------
+"""
+
+import asyncio
+
+
+async def main():
+    print('hello')
+    await asyncio.sleep(1)
+    print(' world')
+
+
+if __name__ == '__main__':
+    asyncio.r
+    pass
+

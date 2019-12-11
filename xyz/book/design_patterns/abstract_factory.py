9a4a5e9d36bf6ecb8b405407ba9bd7bcfad52ca2
cug_xy <cugxy@qq.com>
2019-12-11 15:14:23 +0800

    postgresql practice

---
+++ xyz/book/design_patterns/abstract_factory.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：       abstract_factory.py
+   Description :
+   Author :          cugxy
+   date：            2019/12/3
+-------------------------------------------------
+   Change Activity:
+                     2019/12/3
+-------------------------------------------------
+
+https://design-patterns.readthedocs.io/zh_CN/latest/creational_patterns/abstract_factory.html
+
+"""

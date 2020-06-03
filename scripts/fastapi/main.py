e5f47589b366eb6f9d397bd3c8c33bfddf57057d
cug_xy <cugxy@qq.com>
2020-06-03 17:06:24 +0800

    fast api demo

---
+++ scripts/fastapi/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：       main.py
+   Description :     
+   Author :          cugxy 
+   date：            2020/5/29 
+-------------------------------------------------
+   Change Activity:
+                     2020/5/29 
+-------------------------------------------------
+"""
+
+__author__ = 'cugxy'
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
